de7662d1d400bd79fe6010d7d6c833cc8017bba0
cuidanyang <cuidanyang@bytedance.com>
2016-08-02 17:01:59 +0800

    添加arctemplate，修改main-reportjs

--- target/VisibleServer/assets/scripts/main-report.js
+++ target/VisibleServer/assets/scripts/main-report.js
@@ -1,13 +1,25 @@
 define(function(require, exports, module){
+	require('../css/visual-report.css');
 	$(function() {
+		var edit = '<div class="edit-chart">'
+			+'<div class="chart-title">数据图表标题<div class="chart-desicon"></div></div>'
+			+'<div class="chart-con">'
+			+'<div class="chart-con-add">+</div>'
+			+'<div class="chart-con-view">图标图表图表</div>'
+			+'<div class="chart-con-des">数据图表标题数据图表标题数据图表标题数据图表标题数据图表标题</div>'
+			+'</div>'
+			+'</div>';
 		 $('.report-edit').on('click', '.chart-con-add', function() {
-		        $('.popup').css('display', 'block');
+		        $('.popup-report').css('display', 'block');
 	     });
 		 $().on('click', '.btn-add', function() {
-			
+
 	     });
-	     $('.popup').on('click', '.btn-cancel , .popup-cl', function() {
-		        $('.popup').css('display', 'none');
+	     $('.popup-report').on('click', '.btn-cancel , .popup-cl', function() {
+		        $('.popup-report').css('display', 'none');
 	     });
+		$('.edit-addchart').on('click', function() {
+			$(this).before(edit);
+		});
 	})
 })
